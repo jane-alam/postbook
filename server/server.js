741d61b0054099754b62daa3d13e367e7078de68
jane-alam <janealam416@gmail.com>
2025-07-27 12:53:10 +0600

    Js2

--- server/server.js
+++ server/server.js
@@ -1,4 +1,19 @@
 const express = require('express');
 const mysql = require('mysql');
 const cors = require('cors');
+const app = express();
 const port = 5000;
+
+
+// Middlewares
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send(`Hello World Running on ${port}`);
+})
+
+app.listen(port, () => {
+  console.log(`Server is runnig on ${port}`);
+})
